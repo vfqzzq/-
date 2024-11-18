3532bd6702975394c72acbb15ea1d3a8255858ac
vfqzzq <tiktokvicky0000@gmail.com>
2024-11-19 00:03:13 +0200

    Update Пр3.py

--- Пр3.py
+++ Пр3.py
@@ -1,18 +1,32 @@
-def remove_duplicates(input_list):
-    unique_list = list(set(input_list))
-    return unique_list
+lst = [3,5,6,3,7,4,1,2,3,4,5,6,3,4,5,7,6,5,4,3,4,5,4,3, 'Привіт', "анаконда"]
+print("Список до форматування: ", lst)
+temp_str_list = []
+temp_int_list = []
+def remove_duplicates(a):
+    temp_list = []
+    for i in a:
+        if i not in temp_list:
+            temp_list.append(i)
+    return temp_list
 
-input_list = [1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'анаконда']
 
-def sort_list(input_list):
-    numbers = [x for x in input_list if isinstance(x, (int, float))]
-    strings = [x for x in input_list if isinstance(x, str)]
+def counting(lst_for_counting):
+    for item in lst_for_counting:
+        if type(item) == str:
+            temp_str_list.append(item)
+        elif type(item) == int:
+            temp_int_list.append(item)
+    return temp_int_list, temp_str_list
 
-    numbers.sort()
-    strings.sort(key=lambda x: x.lower())
-    return numbers + strings
 
-unique_list = remove_duplicates(input_list)
-sorted_list = sort_list(unique_list)
+def sortsis(lst_str_for_sort, lst_int_for_sort):
+    sorted = [item.lower() if type(item) == str else item for item in lst_str_for_sort]
+    sorted.sort()
+    lst_int_for_sort.sort()
+    return lst_int_for_sort + sorted
 
-print(sorted_list)
+
+temp_lst = remove_duplicates(a=lst)
+temp_int_list, temp_str_list = counting(temp_lst)
+sortt = sortsis(temp_str_list, temp_int_list)
+print("Список після форматування: ", sortt)
